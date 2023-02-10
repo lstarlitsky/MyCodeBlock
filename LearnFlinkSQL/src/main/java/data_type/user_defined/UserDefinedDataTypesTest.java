683039dad054639f14fe55e1eff31a05fbe35887
lstarlitsky <liu.shunming@foxmail.com>
2023-02-10 17:57:05 +0800

    feat(LearnFlinkSQL): flinksql-用户自定义数据类型

---
+++ LearnFlinkSQL/src/main/java/data_type/user_defined/UserDefinedDataTypesTest.java
@@ -0,0 +1,13 @@
+package data_type.user_defined;
+
+/**
+ * 测试用户自定义数据类型
+ *
+ * @author mit
+ * @date 2023-02-10 10:45
+ */
+public class UserDefinedDataTypesTest {
+    public static void main(String[] args) {
+        
+    }
+}
